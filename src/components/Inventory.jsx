125ebe6043e53631c7497fba400ecb77869db9a0
MilesBradt <smbradtmichael@gmail.com>
2019-11-15 15:46:56 -0800

    changed inventory component function to a class with state

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -5,30 +5,70 @@ import styles from "../css/global.css";
 import inventoryStyles from '../css/inventory.css';
 import stock from '../assets/javascript/inventory.js'
 
-function Inventory(props) {
-    console.log("inventory props: " + props.employee)
-    if (props.employee) {
-        return (
-            <div id="inventoryComponent">
-                <p>Inventory works</p>
-                {stock.map((item, index) =>
-                    <li key={index}>
-                        {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price} [EDIT PLACEHOLDER]
-                    </li>
-                )}
-            </div>
-        )
-    } else {
-        return (
-            <div id="inventoryComponent">
-                <p>Inventory works</p>
-                {stock.map((item, index) =>
-                    <li key={index}>
-                        {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price}
-                    </li>
-                )}
-            </div>
-        )
+// function Inventory(props) {
+//     console.log("inventory props: " + props.employee)
+//     if (props.employee) {
+//         return (
+//             <div id="inventoryComponent">
+//                 <p>Inventory works</p>
+//                 {stock.map((item, index) =>
+//                     <li class="itemsList" key={index}>
+//                         {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price} [{item.amount} pints] <button class="sellButton">Sell</button> / <button id="classButton">Edit</button>
+//                     </li>
+//                 )}
+//             </div>
+//         )
+//     } else {
+//         return (
+//             <div id="inventoryComponent">
+//                 <p>Inventory works</p>
+//                 {stock.map((item, index) =>
+//                     <li class="itemsList" key={index}>
+//                         {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price}
+//                     </li>
+//                 )}
+//             </div>
+//         )
+//     }
+// }
+
+class Inventory extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: stock.name,
+            brand: stock.brand,
+            price: stock.price,
+            alcoholContent: stock.alcoholContent,
+            amount: SVGTextContentElement.amount
+        };
+    }
+
+    render() {
+        console.log("inventory props: " + this.props.employee)
+        if (this.props.employee) {
+            return (
+                <div id="inventoryComponent">
+                    <p>Inventory works</p>
+                    {stock.map((item, index) =>
+                        <li class="itemsList" key={index}>
+                            {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price} [{item.amount} pints] <button class="sellButton">Sell</button> / <button id="classButton">Edit</button>
+                        </li>
+                    )}
+                </div>
+            )
+        } else {
+            return (
+                <div id="inventoryComponent">
+                    <p>Inventory works</p>
+                    {stock.map((item, index) =>
+                        <li class="itemsList" key={index}>
+                            {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price}
+                        </li>
+                    )}
+                </div>
+            )
+        }
     }
 }
 
