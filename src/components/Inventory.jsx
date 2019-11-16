c7c3fc1f1bfd56830e5b0c1da5c2116ef1e317ba
MilesBradt <smbradtmichael@gmail.com>
2019-11-15 16:37:03 -0800

    update inventory state amount on button click

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -5,33 +5,6 @@ import styles from "../css/global.css";
 import inventoryStyles from '../css/inventory.css';
 import stock from '../assets/javascript/inventory.js'
 
-// function Inventory(props) {
-//     console.log("inventory props: " + props.employee)
-//     if (props.employee) {
-//         return (
-//             <div id="inventoryComponent">
-//                 <p>Inventory works</p>
-//                 {stock.map((item, index) =>
-//                     <li class="itemsList" key={index}>
-//                         {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price} [{item.amount} pints] <button class="sellButton">Sell</button> / <button id="classButton">Edit</button>
-//                     </li>
-//                 )}
-//             </div>
-//         )
-//     } else {
-//         return (
-//             <div id="inventoryComponent">
-//                 <p>Inventory works</p>
-//                 {stock.map((item, index) =>
-//                     <li class="itemsList" key={index}>
-//                         {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price}
-//                     </li>
-//                 )}
-//             </div>
-//         )
-//     }
-// }
-
 class Inventory extends React.Component  {
     constructor(props) {
         super(props);
@@ -54,8 +27,10 @@ class Inventory extends React.Component  {
                         <li class="itemsList" key={index}>
                             {item.brand}: {item.name} ({item.alcoholContent}%) - ${item.price} [{item.amount} pints] <button onClick={() => {
                                 stock[index].amount = stock[index].amount - 1
-                                console.log(stock[index].amount)}} 
-                                class="sellButton">Sell</button> / <button id="classButton">Edit</button>
+                                console.log(stock[index].amount)
+                                this.setState({ amount: stock[index].amount })
+                            }} 
+                            class="sellButton">Sell</button> / <button id="classButton">Edit</button>
                         </li>
                     )}
                 </div>
